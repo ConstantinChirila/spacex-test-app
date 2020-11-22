68852a6b514c78310c468ab748d055b48cae27cf
Constantin Chirila <constantin.chirila@gmail.com>
2020-11-22 17:14:26 +0000

    Added Header Component

---
+++ src/components/Header/header.styled.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  max-width: 120rem;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+`;
+
+export const StyledImg = styled.img`
+  max-width: 200px;
+`;
+
+export const StyledLogo = styled.div`
+  font-size: 2rem;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  font-weight: 400;
+  span {
+    margin-left: -1.5rem;
+  }
+`;
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from "react";
+import { StyledHeader, StyledImg, StyledLogo } from "./header.styled";
+import logo from "./../../assets/spacex-logo.png";
+import { Button } from "..";
+
+export function Header(): ReactElement {
+  return (
+    <StyledHeader>
+      <StyledLogo>
+        <StyledImg src={logo} alt="SapceX" />
+        <span>Launches</span>
+      </StyledLogo>
+      <Button onClick={(e) => console.log(e)} isLeftRound>
+        Reload Data
+        <Icon />
+      </Button>
+    </StyledHeader>
+  );
+}
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M20 11A8.1 8.1 0 004.5 9M4 4v5h5M4 13a8.1 8.1 0 0015.5 2m.5 5v-5h-5" />
+    </svg>
+  );
+}
